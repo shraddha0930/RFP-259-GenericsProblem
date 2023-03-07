083e7071f6ce66dd99ab55a86162ae35735f694a
shraddha0930 <shraddhaingole11@gmail.com>
2023-03-08 00:09:08 +0530

    Passed:welcome message

---
+++ MaximumOfThree.java
@@ -0,0 +1,7 @@
+package assignments.GenericsProblem;
+
+public class MaximumOfThree {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Computing Maximum Of Three Numbers Using Java Generics");
+    }
+}
