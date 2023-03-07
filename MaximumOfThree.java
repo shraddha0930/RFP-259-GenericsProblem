cf7abdad3ef58e6595ebeec342119590087f48db
shraddha0930 <shraddhaingole11@gmail.com>
2023-03-08 00:43:59 +0530

    Added:code to find the maximum of three given float number

--- MaximumOfThree.java
+++ MaximumOfThree.java
@@ -18,14 +18,32 @@ public class MaximumOfThree {
 
         System.out.println("The maximum number is: " + maximumNumber);
     }
+    public static void findMaximum(Float firstNum, Float secondNum, Float thirdNum) {
+        Float maximumNumber;
+
+        if(firstNum.compareTo(secondNum)>0 && firstNum.compareTo(thirdNum)>0) {
+            maximumNumber = firstNum;
+        }
+        else if(secondNum.compareTo(firstNum)>0 && secondNum.compareTo(thirdNum)>0) {
+            maximumNumber = secondNum;
+        }
+        else {
+            maximumNumber = thirdNum;
+        }
+
+        System.out.println("The maximum Float number is: " + maximumNumber);
+    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Welcome To Computing Maximum Of Three Numbers Using Java Generics");
-        System.out.println("Enter three number:");
+
+        System.out.println("Enter three  integer number:");
         Integer firstNumber = scanner.nextInt(), secondNumber = scanner.nextInt() , thirdNumber = scanner.nextInt();
         findMaximum(firstNumber ,secondNumber,thirdNumber);
+        System.out.println("Enter three float number:");
+        Float firstNum = scanner.nextFloat(),secondNum = scanner.nextFloat(), thirdNum = scanner.nextFloat();
+        findMaximum(firstNum,secondNum,thirdNum);
     }
 
 }
