db065a3c4dac823cc908d5278c0414689383a3c6
shraddha0930 <shraddhaingole11@gmail.com>
2023-03-08 00:29:22 +0530

    Added:code to find the maximum of three given integer

--- MaximumOfThree.java
+++ MaximumOfThree.java
@@ -1,7 +1,31 @@
 package assignments.GenericsProblem;
 
+import java.util.Scanner;
+
 public class MaximumOfThree {
-    public static void main(String[] args) {
-        System.out.println("Welcome To Computing Maximum Of Three Numbers Using Java Generics");
+    public static void findMaximum(Integer firstNumber, Integer secondNumber, Integer thirdNumber) {
+        Integer maximumNumber;
+
+        if(firstNumber.compareTo(secondNumber)>0 && firstNumber.compareTo(thirdNumber)>0) {
+            maximumNumber = firstNumber;
+        }
+        else if(secondNumber.compareTo(firstNumber)>0 && secondNumber.compareTo(thirdNumber)>0) {
+            maximumNumber = secondNumber;
+        }
+        else {
+            maximumNumber = thirdNumber;
+        }
+
+        System.out.println("The maximum number is: " + maximumNumber);
     }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome To Computing Maximum Of Three Numbers Using Java Generics");
+        System.out.println("Enter three number:");
+        Integer firstNumber = scanner.nextInt(), secondNumber = scanner.nextInt() , thirdNumber = scanner.nextInt();
+        findMaximum(firstNumber ,secondNumber,thirdNumber);
+    }
+
 }
