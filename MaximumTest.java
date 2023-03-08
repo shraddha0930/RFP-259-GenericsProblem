c32ff0a68d0b9d4459c2c1d80d36fc089e7e0e5e
shraddha0930 <shraddhaingole11@gmail.com>
2023-03-08 08:50:25 +0530

    Refactor:code to create generics class to take in 3 variables of generic type

--- MaximumTest.java
+++ MaximumTest.java
@@ -1,11 +1,6 @@
 package assignments.GenericsProblem;
 
-import java.util.Scanner;
-
 public class MaximumTest<T extends Comparable<T>> {
-
-
-
 	public static <E extends Comparable<E>> E maximum(E firstVariable,E secondVariable,E thirdVariable) {
 		E max = firstVariable;
 		if(secondVariable.compareTo(max) > 0)
@@ -20,38 +15,5 @@ public class MaximumTest<T extends Comparable<T>> {
 	}
 }
 
-class MaxTestMain {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-
-		System.out.println("enter number1: ");
-		int intFirst = sc.nextInt();
-		System.out.println("enter number2: ");
-		int intSecond = sc.nextInt();
-		System.out.println("enter number3: ");
-		int intThird = sc.nextInt();
-
-		System.out.println("enter float number1: ");
-		float floatFirst = sc.nextFloat();
-		System.out.println("enter float number2: ");
-		float floatSecond = sc.nextFloat();
-		System.out.println("enter float number3: ");
-		float floatThird = sc.nextFloat();
-
-		System.out.println("enter first string: ");
-		String stringFirst = sc.next();
-		System.out.println("enter second string: ");
-		String stringSecond = sc.next();
-		System.out.println("enter third string: ");
-		String stringThird = sc.next();
-
-		System.out.println("The Maximum Integer Number is: " + MaximumTest.maximum(intFirst,intSecond,intThird));
-		System.out.println("The Maximum Float Number is: " + MaximumTest.maximum(floatFirst, floatSecond, floatThird));
-		System.out.println("The Maximum String is: " + MaximumTest.maximum(stringFirst, stringSecond,stringThird));
-
-
-	}
-
-}
 
